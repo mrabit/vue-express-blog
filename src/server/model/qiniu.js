4d0e909390b8b38250b0a45c288c491472c3960a
mrabit <biabia123456@126.com>
2017-10-23 23:44:26 +0800

    qiniu

--- src/server/model/qiniu.js
+++ src/server/model/qiniu.js
@@ -1,38 +1,50 @@
-var qiniu = require("qiniu")
-var request = require('request')
+var qiniu = require("qiniu");
+var request = require('request');
 var qiniu_config = require('../config')['qiniu'];
+var url = require('url');
 
-//需要填写你的 Access Key 和 Secret Key
-var mac = new qiniu.auth.digest.Mac(qiniu_config.accessKey, qiniu_config.secretKey);
-console.log(mac)
+
+
+var qiniu_model = function () {
+    //需要填写你的 Access Key 和 Secret Key
+    console.log(qiniu_config);
+    this.mac = new qiniu.auth.digest.Mac(qiniu_config.accessKey, qiniu_config.secretKey);
     //要上传的空间
-var bucket = qiniu_config.bucket
-
-//上传到七牛后保存的文件名
-var d = new Date()
-    // var key = 'gm-' + d.getTime() + '.jpg'
-var key = (Date.parse(new Date()) / 1000) + '.jpg'
-
-
-var entry = bucket + ":" + key
-var encodedEntryURI = qiniu.util.urlsafeBase64Encode(entry)
-
-var pic = new Buffer('https://blog.ssliuq.com/Uploads/Picture/2017-10-04/59d4ad87be2c5.jpg').toString('base64')
-var basePic = qiniu.util.base64ToUrlSafe(pic)
-
-var token = qiniu.util.generateAccessToken(mac, 'http://iovip.qbox.me/fetch/' + basePic + '/to/' + encodedEntryURI)
-console.log(token)
-
-var options = {
-    method: 'POST',
-    url: 'http://iovip.qbox.me/fetch/' + basePic + '/to/' + encodedEntryURI,
-    headers: {
-        authorization: token,
-        'content-type': 'application/x-www-form-urlencoded'
-    }
+    this.bucket = qiniu_config.bucket;
 }
 
-request(options, function(error, response, body) {
-    if (error) throw new Error(error)
-    console.log(body)
-})
+module.exports = qiniu_model;
+
+qiniu_model.prototype.fetch_file_by_url = function (filePath, fileName) {
+    //上传到七牛后保存的文件名
+    if (!fileName) {
+        var reg = /.*\/(.*)/;
+        fileName = filePath.replace(reg,"$1");
+        // fileName = (Date.parse(new Date()) / 1000) + '.jpg';
+    }
+
+    var entry = this.bucket + ":" + fileName;
+    var encodedEntryURI = qiniu.util.urlsafeBase64Encode(entry);
+
+    var pic = new Buffer(filePath).toString('base64')
+    var basePic = qiniu.util.base64ToUrlSafe(pic)
+
+    var url = 'http://iovip.qbox.me/fetch/' + basePic + '/to/' + encodedEntryURI;
+
+    var token = qiniu.util.generateAccessToken(this.mac, url);
+
+    var options = {
+        method: 'POST',
+        url: url,
+        headers: {
+            authorization: token,
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+    }
+    return new Promise((resolve, reject) => {
+        request(options, function (error, response, body) {
+            if (error) reject(error);
+            resolve(body);
+        })
+    })
+}
--- src/server/router/qiniuController.js
+++ src/server/router/qiniuController.js
@@ -1,10 +1,12 @@
-// var Upload = require('../model/qiniu');
+var qiniu_model = require('../model/qiniu');
 var express = require('express');
 var router = express.Router();
 
 router.get('/upload', function(req, res) {
-    require('../model/qiniu');
-    res.end("success");
+    var qiniu = new qiniu_model();
+    qiniu.fetch_file_by_url("https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png").then(result => {
+        res.json(result);
+    })
 })
 
 module.exports = router;
